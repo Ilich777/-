ebf71a42f5bd0b77b9206970708bd495d3ee1a3b
IZhukov <IZhukov@TP-VI-IV>
2022-09-30 11:29:58 +0600

    Небольшой рефакторинг

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let name = prompt('Привет! Как тебя зовут?')
+let greeting = 'Привет! Как тебя зовут?'
+let name = prompt(greeting)
 while (name == '' || name == null) {
-    name = prompt('Привет! Как тебя зовут?')
+    name = prompt(greeting)
 }
 let rules = `${name}, я загадал число от 1 до 100. Попробуй отгадать его за наименьшее число попыток. 
 После каждой попытки я скажу 'мало', 'много' или 'угадал'.`
@@ -8,20 +9,12 @@ alert(rules)
 
 let number = random(100)  // число, которое загадал компьютер
 let guess = prompt('Какое число я загадал?')
-let amountAttempt =0//количество попыток
+let amountAttempt =1 //количество попыток
 
 while (guess != number) {
-    if (number < guess) {
-        guess = prompt('Много. Попробуй ещё раз')
-
-        amountAttempt++
-    }
-
-    if (number > guess) {
-        guess = prompt('Мало. Попробуй ещё раз')
-
-        amountAttempt++
-    }
+    amountAttempt++
+    if (number < guess) guess = prompt('Много. Попробуй ещё раз') 
+    else if (number > guess) guess = prompt('Мало. Попробуй ещё раз')
 }
 
 alert('Ты угадал! Это число '+ number + '. Количество потраченных попыток - ' + amountAttempt + '.')
