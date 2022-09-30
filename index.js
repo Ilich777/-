e0ad446e38e8147c27d07d18650c970d11a818a8
IZhukov <IZhukov@TP-VI-IV>
2022-09-30 09:02:30 +0600

    Число которое загадано - статично. Работает проверка числа

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let name = prompt('Привет! Как тебя зовут?')
-if (name == '' || name == null) {
+while (name == '' || name == null) {
     name = prompt('Привет! Как тебя зовут?')
 }
 let rules = `${name}, я загадал число от 1 до 100. Попробуй отгадать его за наименьшее число попыток. 
 После каждой попытки я скажу 'мало', 'много' или 'угадал'.`
 alert(rules)
 
+let number = 42 // число, которое загадал компьютер
+let guess = prompt('Какое число я загадал?')
 
+while (guess != number) {
+    if (number < guess) guess = prompt('Много. Попробуй ещё раз')
+    if (number > guess) guess = prompt('Мало. Попробуй ещё раз')
+}
 
-
+alert('Ты угадал! Это число '+ number)
